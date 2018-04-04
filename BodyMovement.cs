ab8fb35e37f7ea2262e55bcd1ba77a65db876b89
jakeparcell14 <jakeparcell14@gmail.com>
2018-04-03 22:09:46 -0700

    Add files via upload

---
+++ BodyMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyMovement : MonoBehaviour
+{
+
+    public GameObject player;
+
+    public GameObject leader;
+
+    private Rigidbody rb;
+
+    public int position;
+
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - leader.transform.position;
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    // Update is run every frame but after all items have been processed in Update
+    void LateUpdate()
+    {
+        //transform.position = leader.transform.position + offset;
+
+        player = GameObject.Find("Player");
+        PlayerController p = player.GetComponent<PlayerController>();
+
+        if (p.length >= position)
+        {
+            gameObject.GetComponent<MeshRenderer>().enabled = true;
+        }
+    }
+
+}
---
+++ PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public int length;
+
+    private Vector3 movement;
+    public Vector3 lastMovement;
+
+    public Text countText;
+    public Text winText;
+
+    public float speed;
+    private int count;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+
+        length = 1;
+    }
+
+
+    //called before physics calculations
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        lastMovement = movement;
+
+        movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count+= 1;
+            SetCountText();
+            length+= 1;
+        }
+
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 6)
+        {
+            winText.text = "You Win!!";
+        }
+    }
+}
